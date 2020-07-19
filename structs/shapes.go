5c5c5a0c5f411e4a7e13df6a0c125a8f72ea3b55
danizavtz <daniellucena@yahoo.com.br>
2020-07-19 10:30:32 -0300

    Inclusão de nova funcionalidade

---
+++ structs/shapes.go
@@ -0,0 +1,31 @@
+package main
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width float64
+	Heigth float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Heigth
+}
+
+type Circle struct {
+	Radius float64
+}
+func (c Circle) Area() float64{
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64{
+	return 2 * (rectangle.Width + rectangle.Heigth)
+}
+
+func Area(rectangle Rectangle) float64{
+	return rectangle.Width * rectangle.Heigth
+}
+
