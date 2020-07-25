a51a101bf4fa035d49bdb48288cc9012654323ac
danizavtz <daniellucena@yahoo.com.br>
2020-07-25 07:38:04 -0300

    Insercao de injecao de dependencia

---
+++ dependencyinjection/dependency.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "os"
+import "io"
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+}
