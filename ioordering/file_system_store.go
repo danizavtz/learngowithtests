46142db82c82f0f8c6a6d0937885a6ce39ea7482
danizavtz <daniellucena@yahoo.com.br>
2020-08-09 14:00:52 -0300

    Inserção de testes de unidade

--- ioordering/file_system_store.go
+++ ioordering/file_system_store.go
@@ -3,13 +3,28 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"fmt"
 )
 type FileSystemPlayerStore struct {
 	database io.Reader
 }
 
+type ReadSeeker interface {
+	Reader
+	Seeker
+}
+
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	var league []Player
-	json.NewDecoder(f.database).Decode(&league)
+	league, _ := NewLeague(f.database)
 	return league
+}
+
+func NewLeague(rdr io.Reader) ([]Player, error) {
+	var league []Player
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsin league, %v", err)
+
+	}
+	return league, err
 }
